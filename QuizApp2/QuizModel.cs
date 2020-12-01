5e4755176b2141d43fd65327b3cdbc925359e761
IonZource <dao47@live.com>
2020-12-01 16:22:36 -0600

    from work

---
+++ QuizApp2/QuizModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuizApp2
+{
+    sealed class Question
+    {
+        public int questionNumber { get; set; }
+        public string questiontext { get; set; }
+        public string answer1 { get; set; }
+        public string answer2 { get; set; }
+        public string answer3 { get; set; }
+        public string answer4 { get; set; }
+        public string correctAnswer { get; set; }
+    }
+}
